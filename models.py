5e7cd5af4874ce673040808cebcd725984eccc95
EltonLaw <eltonlaw296@gmail.com>
2017-07-20 02:28:16 -0400

    added importing/exporting of tf metagraph def protobufs

--- models.py
+++ models.py
@@ -1,10 +1,24 @@
+""" Build and export TensorFlow computational graphs """
+import os
+# Quiets TensorFlow compile errors
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# pylint: disable=wrong-import-position
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 
 
-def experiment(ps):
-    mnist = input_data.read_data_sets('../../../MNIST_data', one_hot=True)
-    tf.set_random_seed(ps.seed)
+# pylint: disable=invalid-name
+def test_experiment(FLAGS):
+    """ Test Experiment
+
+    PARAMETERS
+    ----------
+    FLAGS: Dictionary of model parameters
+
+    RETURNS
+    -------
+    TensorFlow MetaGraphDef proto
+    """
+    tf.set_random_seed(FLAGS.seed)
 
     x = tf.placeholder(tf.float32, shape=[None, 784])
     y_ = tf.placeholder(tf.float32, shape=[None, 10])
@@ -14,24 +28,28 @@ def experiment(ps):
 
     y = tf.matmul(x, W) + b
 
-    cross_entropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    train_op = tf.train.GradientDescentOptimizer(ps.lr).minimize(cross_entropy)
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+    train = tf.train.GradientDescentOptimizer(FLAGS.lr).minimize(cost)
+
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    tf.summary.scalar("accuracy", accuracy)
+
+    score = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    tf.summary.scalar("score", score)
 
     merged_summary = tf.summary.merge_all()
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(ps.epochs):
-            batch = mnist.train.next_batch(ps.batch_size)
-            _, s, a = sess.run([train_op, merged_summary, accuracy],
-                               feed_dict={x: batch[0], y_: batch[1]})
-            if epoch % ps.save_step == 0:
-                print("Epoch {} Training Error = {}".format(epoch+1, a))
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y_', y_)
+    tf.add_to_collection('train_op', train)
+    tf.add_to_collection('score_op', score)
+    tf.add_to_collection('summary_op', merged_summary)
+    collection_list = tf.get_default_graph().get_all_collection_keys()
 
-        test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images,
-                                                 y_: mnist.test.labels})
-    return test_accuracy
+    # g.finalize()
+    meta_graph = tf.train.export_meta_graph(
+        collection_list=collection_list
+    )
+    return meta_graph
--- paramsearch.sh
+++ paramsearch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Makes a master logs directory if it doesn't already exist
 mkdir -p logs
@@ -7,9 +7,11 @@ current_date=$(date "+%Y-%m-%d-%H_%M_%S")
 log_dir="logs/$current_date"
 mkdir $log_dir
 # Create a file to aggregate all the accuracy results
-touch "$log_dir/results.txt"
+# touch "$log_dir/results.txt"
 
 # Run Experiments
-python3 run.py --batch_size=64 --log_dir=$log_dir
-python3 run.py --batch_size=128 --log_dir=$log_dir
-python3 run.py --batch_size=256 --log_dir=$log_dir
+python3 run.py --batch_size=64 --log_dir=$log_dir >> $log_dir/results.txt
+# python3 run.py --batch_size=128 --log_dir=$log_dir >> $log_dir/results.txt
+# python3 run.py --batch_size=256 --log_dir=$log_dir >> $log_dir/results.txt
+
+echo "== Job Complete == "
